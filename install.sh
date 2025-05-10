4e496133de9f2eabaaf66824b2d76f25c2b0f21d
Isaiah Bjorklund <isaiah98981@icloud.com>
2025-05-10 12:44:16 -0700

    Fix install.sh to correct pip install command by removing duplicate 'datasets' package

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 apt-get update && apt-get install -y git python3-pip \
 
 pip install --upgrade pip && \
-pip install torch==2.5.1 transformers[torch] trl peft bitsandbytes accelerate wandb huggingface_hub datasets pillow xformers && \
+pip install torch==2.5.1 transformers[torch] datasets trl peft bitsandbytes accelerate wandb huggingface_hub datasets pillow xformers && \
 pip install unsloth_zoo && \    
 pip install ninja packaging wheel hf_xet
 
